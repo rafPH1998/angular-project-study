b00b8e790f64c63d38cb052bea44c83c4e30feaf
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-08-13 00:54:49 -0300

    entendendo conceitos de props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular';
+    userName: string = 'Joaquim'
+
+    userData = {
+        email: 'joaquim@gmail.com',
+        role: 'ADMIN',
+    };
+
+    title = 'curso-angular';
 }
