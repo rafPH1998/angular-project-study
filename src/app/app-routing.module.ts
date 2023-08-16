26bd11c4a7f6e87aba6cc6248fa4fdd2b80b02a8
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-08-15 21:58:19 -0300

    RouterLink para rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { ListRenderComponent } from "./components/list-render/list-render.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: FirstComponentComponent},
+    {path: 'list', component: ListRenderComponent},
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
